6baf5379c8e4970f9d5fd5682dcce8bf6efe7d43
Ambjorn97 <turpuhanov.amir@yandex.ru>
2024-10-06 21:47:12 +0300

    задача решена

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,5 +1,6 @@
 package koschei.config;
 
+import koschei.models.Deth8;
 import koschei.models.Island2;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
@@ -7,11 +8,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "koschei")
+@ComponentScan("koschei")
 public class AppConfig {
 
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
+    }
+    @Bean
+    public Deth8 getDeth() {
+        return new Deth8();
     }
 }
